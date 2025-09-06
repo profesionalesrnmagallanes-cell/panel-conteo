af25b21855f2d7b5d018f299a50302681dbb2acb
Cesar <profesionalesrnmagallanes@gmail.com>
2025-09-05 23:17:38 -0300

    feat: eliminar archivo de registro de depuración y actualizar configuración de Firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdNymCz1L_pAygh7m7hJr7Ib6nDShUixw",
+  authDomain: "conteo-en-vivo.firebaseapp.com",
+  projectId: "conteo-en-vivo",
+  storageBucket: "conteo-en-vivo.appspot.com",
+  messagingSenderId: "161248825790",
+  appId: "1:161248825790:web:5c53d8c6aa1849f157e0fc",
+  measurementId: "G-4B1HE3BC4P"
+};
